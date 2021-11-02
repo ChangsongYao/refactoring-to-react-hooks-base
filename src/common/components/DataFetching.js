cfb5a9180edbafa7bf878b36a1ebaff7548fd663
yaochangsong <2543144515@qq.com>
2021-11-03 00:39:00 +0800

    To keep things clean we extract a function to build the UI depending on the state:

--- src/common/components/DataFetching.js
+++ src/common/components/DataFetching.js
@@ -5,15 +5,27 @@ import Loading from './Loading'
 
 const DataFetching = ({ endPoint }) => {
     const { loading, error, data } = useFetch(endPoint)
-    return loading ? (<Loading />) : (
-        <ul>
-            {
-                data.map(element => (
-                    <li key={element.timestamp}>{element.timestamp}-{element.amount}</li>
-                ))
-            }
-        </ul>
-    )
+    /*
+    1. show the loading indicator while loading
+    2. if not loading and errored, show an error message
+    3. if not loading and not errored show the list
+   */
+    const buildUI = () => {
+        if (loading) return <Loading />;
+        if (loading || error) return <p>Ops! Something went wrong: {error}</p>;
+        if (!loading && !error)
+            return (
+                <ul>
+                    {data.map(element => (
+                        <li key={element.timestamp}>
+                            {element.timestamp} - {element.amount}
+                        </li>
+                    ))}
+                </ul>
+            );
+    };
+
+    return buildUI();
 }
 
 DataFetching.propTypes = {
