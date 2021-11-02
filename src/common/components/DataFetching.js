9654e193d36a9eccefb2c6ac593bbfe7f1e2b9bc
yaochangsong <2543144515@qq.com>
2021-11-03 00:37:46 +0800

    add loading

--- src/common/components/DataFetching.js
+++ src/common/components/DataFetching.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import useFetch from '../../hooks/useFetch'
+import Loading from './Loading'
 
 const DataFetching = ({ endPoint }) => {
-    const { data } = useFetch(endPoint)
-    return (
+    const { loading, error, data } = useFetch(endPoint)
+    return loading ? (<Loading />) : (
         <ul>
             {
                 data.map(element => (
