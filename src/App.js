212d1138f0602645c9a0bb25c025fc0b8a05a718
yaochangsong <2543144515@qq.com>
2021-11-14 23:46:40 +0800

    remove all redux and delete DataFetchingContainer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React, { useContext, createContext } from "react";
 import DashboardShell from "./features/Dashboard/DashboardShell";
-import DataFetchingContainer from "./features/Playground/DataFetchingContainer";
+// import DataFetchingContainer from "./features/Playground/DataFetchingContainer";
 import { sales } from "./mocks";
+
+
+if (process.env.NODE_ENV === "development") {
+  console.log("development")
+  const { Server } = require("miragejs");
+  const { sales, subscriptions } = require("./mocks");
+  new Server({
+    routes() {
+      this.namespace = process.env.REACT_APP_BASE_URL;
+      this.get("sales/", () => sales);
+      this.get("subscription/", () => subscriptions);
+    }
+  })
+}
+
+
 
 const initialState = {
   loading: false,
